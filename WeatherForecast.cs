fd988d8aaa2a5d6cda0d0068edb0ed1b58f5acb4
Ritesh Patel <ritesh.patel121@gmail.com>
2023-01-13 17:23:04 +0000

    Initial Commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Azure_DevOps_Provision_API_Infrastructure_using_Terraform;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
